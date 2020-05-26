ee3a2e14c2638466d2eab4d2078f79a091768171
lijundong <leeonfield@foxmail.com>
2020-05-26 21:20:39 +0800

    :sparkles: init video plugin

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const videoMap = {
+  youku: "http://player.youku.com/embed/",
+  bilibili: "http://player.bilibili.com/player.html?aid=",
+  qq: "https://v.qq.com/txp/iframe/player.html?vid=",
+  youtube: "https://www.youtube.com/embed/",
+};
+
+const renderVideo = (wrapperId, sourceId, type) => {
+  console.log(wrapperId, sourceId, type);
+
+  var el = document.querySelector("#" + wrapperId);
+  if (type && videoMap[type]) {
+    const url = videoMap[type];
+    console.log(".....", url);
+    el.innerHTML = `<iframe height=450 width=640 align='center' src='${url}${sourceId}' frameborder=0 'allowfullscreen'></iframe>`;
+  }
+};
+
+export default function videoPlugin(editor, options = {}) {
+  const { codeBlockManager } = Object.getPrototypeOf(editor).constructor;
+  const vList = options.vList || {};
+  const videoList = {
+    ...videoMap,
+    ...vList,
+  };
+
+  Object.keys(videoList).forEach((type) => {
+    codeBlockManager.setReplacer(type, function (sourceId) {
+      if (!sourceId) return;
+      var wrapperId = type + Math.random().toString(36).substr(2, 10);
+      setTimeout(renderVideo.bind(null, wrapperId, sourceId, type), 0);
+      return `<div style="text-align: center" id="${wrapperId}"></div>`;
+    });
+  });
+}
