6d65f3db247d9007858932e0672e73cd51aca64c
lijundong <leeonfield@foxmail.com>
2020-08-20 11:23:04 +0800

    :sparkles: 增加 video source 视频类型

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ const defaultList = {
   bilibili: 'http://player.bilibili.com/player.html?aid=',
   qq: 'https://v.qq.com/txp/iframe/player.html?vid=',
   youtube: 'https://www.youtube.com/embed/',
+  'source-mp4': '',
+  'source-ogg': '',
+  'source-avi': '',
 };
 
 const renderVideo = (wrapperId, sourceId, type, videoMap) => {
-  let el = document.querySelector('#' + wrapperId);
+  var el = document.querySelector('#' + wrapperId);
 
-  if (type && videoMap[type]) {
+  if (type) {
     const url = videoMap[type];
 
-    el.innerHTML = `<iframe height=450 width=640 align='center' src='${url}${sourceId}' frameborder=0 allowfullscreen></iframe>`;
+    if (url) {
+      el.innerHTML = `<iframe height=450 width=640 align='center' src='${url}${sourceId}' frameborder=0 allowfullscreen></iframe>`;
+    } else if (/^source-/.test(type)) {
+      const videoType = type.replace('source-', '');
+
+      el.innerHTML = `<video width="640" controls  src="${sourceId}" type="video/${videoType}"></video>`;
+    }
   }
 };
 
 export default function videoPlugin(editor, options = {}) {
-  const vList = options.list || {};
   const { codeBlockManager } = Object.getPrototypeOf(editor).constructor;
+  const vList = options.vList || {};
   const videoMap = {
     ...defaultList,
     ...vList,
